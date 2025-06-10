7f8c0b5af5b17d54a3c783a1542f34606161e2da
Arne Hendricks <arne@planbar.app>
2025-06-10 13:15:37 +0200

    chore(scim-server): Change test suite execution and add comment about problem
    https://git.knut.univention.de/univention/dev/internal/team-nubus/-/issues/1179

--- docker/scim-server/test-entrypoint.sh
+++ docker/scim-server/test-entrypoint.sh
@@ -1,10 +1,23 @@
-#!/bin/sh
-# SPDX-License-Identifier: AGPL-3.0-only
-# SPDX-FileCopyrightText: 2025 Univention GmbH
+#!/bin/bash
+ # SPDX-License-Identifier: AGPL-3.0-only
+ # SPDX-FileCopyrightText: 2025 Univention GmbH
 
 
-cd scim-server
-uv sync
-uv run pytest -lv $@ tests/unit/
-uv run pytest -lv $@ tests/e2e/
-uv run pytest -lv $@ tests/middlewares/
+ cd scim-server
+ uv sync
+
+# FIXME: tests need to be run in isolation because when they are run all in the same session some are failing
+# Examples of tests that fail in current run order if being ran in one session: (changes if execution is changed)
+# * tests/unit/test_user.py::TestUserAPI::test_level4_filter_expression
+# * tests/unit/test_user.py::TestUserAPI::test_level4_remove_with_filter
+# * tests/unit/test_user.py::TestUserAPI::test_level5_pathless_operation
+test_suites=(
+  tests/unit/
+  tests/e2e/
+  tests/middlewares/
+)
+
+for test_suite in "${test_suites[@]}"
+do
+  uv run pytest "${test_suite}" || exit -1
+done
