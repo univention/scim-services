71a7abe39dbdc8b96b98a61a4310ad2372d9dafd
Daniel Troeder <troeder@univention.de>
2025-06-16 11:28:10 +0200

    ci: fix paths in coverage report

--- docker/scim-server/test-entrypoint.sh
+++ docker/scim-server/test-entrypoint.sh
@@ -1,10 +1,13 @@
-#!/bin/bash
+#!/bin/sh
 # SPDX-License-Identifier: AGPL-3.0-only
 # SPDX-FileCopyrightText: 2025 Univention GmbH
 
+set -x
 
-cd scim-server
-uv sync
+# Run pytest and coverage from /app to get proper paths in coverage report.
 
-uv run pytest -p no:cacheprovider tests/ $@
-uv run coverage xml --ignore-errors
+# coverage seems to ignore "pytest --config-file=" and "coverage --rcfile=", so passing in everything explicitly.
+
+uv sync --directory scim-server
+uv run pytest --config-file=/app/scim-server/pyproject.toml --cov-config=/app/scim-server/pyproject.toml -p no:cacheprovider scim-server/tests $@
+uv run coverage xml --rcfile=/app/scim-server/pyproject.toml --ignore-errors --data-file=/tmp/scim-coverage -o /tmp/scim-coverage.xml
