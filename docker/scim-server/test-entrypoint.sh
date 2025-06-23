8e71293fd7b69708887fd13e72c5ae04f69c29c4
Andreas Kosubek <andreas.kosubek.extern@univention.de>
2025-06-23 12:32:07 +0200

    fix(Sonarqube): Modified coverage call and pipeline configuration.
    univention/dev/projects/scim/scim-services#2

--- docker/scim-server/test-entrypoint.sh
+++ docker/scim-server/test-entrypoint.sh
@@ -9,5 +9,4 @@ set -x
 # coverage seems to ignore "pytest --config-file=" and "coverage --rcfile=", so passing in everything explicitly.
 
 uv sync --directory scim-server
-uv run pytest --config-file=/app/scim-server/pyproject.toml --cov-config=/app/scim-server/pyproject.toml -p no:cacheprovider scim-server/tests $@
-uv run coverage xml --rcfile=/app/scim-server/pyproject.toml --ignore-errors --data-file=/tmp/scim-coverage -o /tmp/scim-coverage.xml
+uv run pytest --config-file=/app/scim-server/pyproject.toml --cov-config=/app/scim-server/pyproject.toml --cov-report=xml:/tmp/scim-coverage.xml -p no:cacheprovider scim-server/tests $@
